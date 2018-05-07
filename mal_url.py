f189db86732995b09504b9019d1aac8a69168951
Nathan Dullea <njdullea@gmail.com>
2018-05-06 22:28:58 -0500

    Remove Unnecessary Files

--- mal_url.py
+++ mal_url.py
@@ -1,19 +1,14 @@
+#Nathan Dullea
+#Artificial Intelligence Project
+#Using Sklearn to Classify Malicious URLs
+
 import pandas as pd
 import numpy as np
 import sklearn
 import random
-#For exporting trained classifier
-import pickle
-
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-#from sklearn import linear_model
 from sklearn.linear_model import LogisticRegression
-
-
-import matplotlib.pyplot as plt
-
 
 #Use Bag of Words approach for tokenizing
 def makeTokens(input):
@@ -31,7 +26,7 @@ def makeTokens(input):
 					tokenList = tokenList + [l]
 
 
-	#Turn to set to remove redundancies
+	#Turn to set to remove redundancies and back to List
 	tokenList = list(set(tokenList))
 
 	#Remove com and com' from tokenList
@@ -40,68 +35,31 @@ def makeTokens(input):
 	if "com'" in tokenList:
 		tokenList.remove("com'")
 
-	#firstToken = tokenList[:1]
-	#print(firstToken)
-
 	return tokenList
 
 
 def setupDataAndClassifier():
-	#Read Data from csv
-	#headings = ['url', 'label']
+	#Read Data from csv into DataFrame
 	data = pd.read_csv('/Users/nathandullea/Desktop/data.csv')
-	#X = data['url']
-	#y = data['label']
-	#print(X)
-	#print(y)
-
-	#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-
-	#lgs = LogisticRegression()
-	#lgs.fit(X_train, y_train)
-	#print(lgs.score(X_test, y_test))
-	#Convert to framework (csv already returns framework though?)
-	
-	dataFrame = pd.DataFrame(data)
 
 	#Convert to Array and randomize data
-	alldata = np.array(dataFrame)
+	alldata = np.array(data)
 	random.shuffle(alldata)
 
-	#Setup Training and Testing Date
-	labels = [d[1] for d in alldata]
+	#Setup Training and Testing Data
+	y = [d[1] for d in alldata]
 	urls = [d[0] for d in alldata]
+	#tfidfvectorizer is equivalent to countvectorizer follower by tfidf transform
 	vectorizer = TfidfVectorizer(tokenizer=makeTokens)
 	X = vectorizer.fit_transform(urls)
-	#y = vectorizer.fit_transform(labels)
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-	#print(len(X))
-	#print(len(urls))
-	#print(len(y))
 
-
-	#Train Logisitc Regression Model
+	#Create Logistic Regression Model
 	lgs = LogisticRegression()
+	#Train Logisitc Regression Model
 	lgs.fit(X_train, y_train)
+	#Print the Score
 	print(lgs.score(X_test, y_test))
-
-	#Export to Pickle
-
-	#predict on X_test set
-	y_pred = lgs.predict(X_test)
-
-	#Plot Logistic Regression with matplotlib
-	#plt.scatter(X_test, y_test)
-	#plt.plot(X_test, y_pred, color='blue', linewidth=5)
-	#plt.xticks(())
-	#plt.yticks(())
-	#plot.scatter()
-	#plt.show()
-	
-	#pyplot.scatter(urls, y)
-	
-
 
 
 setupDataAndClassifier()
